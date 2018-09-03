674aee5dbf753656b05413eae785c1db73d0e2e1
Sam Ramachandran <spvram74@gmail.com>
2018-09-02 20:31:30 -0400

    Commit with handled exception

--- scrape.js
+++ scrape.js
@@ -2,44 +2,35 @@ const rp = require('request-promise');
 const cheerio = require('cheerio');
 var options;
 const moment = require('moment');
-for(var i = 0; i < 100; i++) {
+for(var i = 0; i < 50; i++) {
   (function(i) {
-  var currentDate = moment(new Date()).add(-(i), 'days').format('D-MM-YYYY');
-  //console.log(currentDate);
-  //currentDate = moment(new Date()).add(-1, 'days').format('D-MM-YYYY');
-  //console.log(currentDate);
-  //console.log(yesterday);
-  var requestUrl = "https://oddanchatramvegetablemarket.net/oddanchatram-vegetable-market-price-details-" + currentDate + "/";
-  //console.log(requestUrl);
-  options = {
-    uri: requestUrl,
-    transform: function (body) {
-      return cheerio.load(body);
-    }
-  };
-  rp(requestUrl, function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-      var $ = cheerio.load(html);
-      var b = currentDate + ", ";
-      var counter = 1;
-      $('td').each(function(j, element) {
-        var a = $(this);
-        b = b + a.text().trim(); 
-        //create document for database
-        if ((j + 1) % 3 === 0 ) { 
-          console.log(b);
-          //Store in database
-          b = currentDate + ", ";
-        } else {
-          b = b + ", ";
-        }
-      });
-    } else {
-      console.log(" No data for this date");
-    }
-  });
-})(i);
+    var currentDate = moment(new Date()).add(-(i), 'days').format('D-MM-YYYY');
+    var requestUrl = "https://oddanchatramvegetablemarket.net/oddanchatram-vegetable-market-price-details-" + currentDate + "/";
+    options = {
+      uri: requestUrl,
+      transform: function (body) {
+        return cheerio.load(body);
+      }
+    };
+    rp(options)
+      .then(function($) {
+        var b = currentDate + ", ";
+        var counter = 1;
+        $('td').each(function(j, element) {
+          var a = $(this);
+          b = b + a.text().trim(); 
+          //create document for database
+          if ((j + 1) % 3 === 0 ) { 
+            console.log(b);
+            //Store in database
+            b = currentDate + ", ";
+          } else {
+            b = b + ", ";
+          }
+        });
+    })
+    .catch(function(error) {
+      //console.log("crawl failed with no data for date: " + currentDate);
+    });
+  })(i);
 }
-//.catch(function(err){
- // console.log(requestUrl + " " + " crawling failed");
-//});
